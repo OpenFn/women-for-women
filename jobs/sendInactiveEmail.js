74ec428640866a71dc3641a5577d7f58bc542179
Aleksa Krolls <aleksagk@gmail.com>
2020-11-06 10:41:17 +0200

    aleksa@openfn.org's change on OpenFn.org

--- jobs/sendInactiveEmail.js
+++ jobs/sendInactiveEmail.js
@@ -14,7 +14,7 @@ alterState(state => {
   //each( //update to handle multiple employees
   //  '$.employees[*]',
   
-  //-- SEND WELCOME EMAIl --
+  //-- SEND INACTIVE EMAIl --
   send(
       fields(
         field('from', 'womenforwomen@irc.openfn.org'), //to update with womenforwomen domain
