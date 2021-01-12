4d6cf91cbf6265b45001193797c65d4230ce13d2
Aleksa Krolls <aleksagk@gmail.com>
2021-01-12 15:23:01 +0200

    aleksa@openfn.org's change on OpenFn.org

--- jobs/sendSupervisorEmail.js
+++ jobs/sendSupervisorEmail.js
@@ -13,7 +13,7 @@ each(
       WOC: 'WOC_HR_Notifications@womenforwomen.org',
     };
 
-    const activeDivisions = ['No Division', 'Headquarters', 'Headquarters - PM Access', 'Nigeria', 'Nigeria - PM Access']; // Add divisions to turn "on"
+    const activeDivisions = [ 'Headquarters', 'Headquarters - PM Access', 'Nigeria', 'Nigeria - PM Access']; // Add divisions to turn "on"
 
     const employee = state.data; // We get the current employee
     state.workEmail = employee.fields['Work Email'];
