1fe0b22c85532e272133e6ee779cb6aeda7773f7
jedbgold <71792525+jedbgold@users.noreply.github.com>
2021-01-12 00:51:01 -0500

    maverbuj@womenforwomen.org's change on OpenFn.org

--- jobs/sendSupervisorEmail.js
+++ jobs/sendSupervisorEmail.js
@@ -28,7 +28,7 @@ each(
       return send(
         fields(
           field('from', 'womenforwomen@irc.openfn.org'), //TODO: replace with WfW domain
-          field('to', 'MAverbuj@womenforwomen.org'), //TODO: replace with L30
+          field('to', 'MatiasA@womenforwomen.org'), //TODO: replace with L30
           field('cc', 'aleksa@openfn.org, jed@openfn.org'), //TODO: replace with L31
           //field('to', `${state.superEmail}`), //TODO: use when ready to send TO Division contact
           //field('cc', `${divisionEmailMap[employee.fields.Division]}`), //TODO: use when ready to copy Division contactk
