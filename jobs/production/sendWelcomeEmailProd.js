a4545e67ef019ac7605c799eda739e0c5b580e44
jedbgold <71792525+jedbgold@users.noreply.github.com>
2021-02-08 14:39:21 -0500

    maverbuj@womenforwomen.org's change on OpenFn.org

--- jobs/production/sendWelcomeEmailProd.js
+++ jobs/production/sendWelcomeEmailProd.js
@@ -72,7 +72,7 @@ each(
         border="0"
         cellspacing="0"
         cellpadding="0"
-        bgcolor="#eeeeee"
+        bgcolor="#FFFFFF"
       >
         <tr>
           <td align="center" valign="top">
