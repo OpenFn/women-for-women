05b36ec5adae6764c2796efc3d5fe014227f1f8d
jedbgold <71792525+jedbgold@users.noreply.github.com>
2021-05-17 18:19:34 -0400

    maverbuj@womenforwomen.org's change on OpenFn.org

--- jobs/production2/sendInactiveEmailProd.js
+++ jobs/production2/sendInactiveEmailProd.js
@@ -39,7 +39,7 @@ each(
     state.supervisor = employee.fields['Supervisor name'];
     state.superEmail = employee.fields['Supervisor email'];
     state.endDate = employee.fields['Termination Date'];
-    state.termination = state.endDate.toISOString();
+    state.termination = endDate.toISOString();
     console.log(state.name, state.workEmail, state.division, state.endDate, state.supervisor, state.superEmail);
 
     var msg = `<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
