f47f2bc15e4ca494710361129ce916531e6ffe90
mamadou <mamadou.lakhassane@gmail.com>
2021-05-25 14:33:52 +0000

    fetch file and post to openfninbox, #41

--- jobs/committed/1.fetchJSON.js
+++ jobs/committed/1.fetchJSON.js
@@ -13,38 +13,58 @@ alterState(state => {
       file => fileNames.includes(file.name.split('.')[0].toLowerCase()) && file.name.split('.')[1] === 'csv'
     );
 
-    console.log('Files are:', files);
+    if (files.length === 0) console.log('No new CSV files found.');
 
     return { ...state, files };
   });
 });
 
-//   each(
-//     '$.files[*]',
-//     alterState(state => {
-//       const { configuration, data } = state;
-//       return getJSON(`/UP/${data.name}`)(state).then(state => {
-//         const splitName = data.name.split('.');
-//         const type = data.name.includes('Extract')===true ? 'extract' :
-//           data.name.includes('Component')===true ? 'component' : splitName[0];
-//         const fileContent = {
-//           fileName: data.name,
-//           fileType: type,
-//           json: JSON.parse(state.data),
-//           uploadDate: new Date(data.modifyTime).toISOString(),
-//           siteCode: splitName[0].substring(splitName[0].length - 4),
-//         };
-//         return http
-//           .post({
-//             url: configuration.openfnInboxUrl,
-//             data: fileContent,
-//             maxContentLength: Infinity,
-//             maxBodyLength: Infinity,
-//           })(state)
-//           .then(() => {
-//             console.log(`Posted ${data.name} to OpenFn Inbox.\n`);
-//             return { configuration, references: [] };
-//           });
-//       });
-//     })
-//   );
+each(
+  '$.files[*]',
+  alterState(state => {
+    const { configuration, data } = state;
+    return getCSV(`/${data.name}`)(state).then(state => {
+      const splitName = data.name.split('.');
+      console.log(state.data.length);
+      let json = [];
+      let headers = state.data[0].split(',');
+      headers = headers.map(h => (h = h.replace(/"/g, '')));
+
+      state.data.forEach(data => {
+        let row = data.split(',');
+
+        let obj = {};
+        for (let j = 0; j < row.length; j++) {
+          obj[headers[j]] = row[j].replace(/"/g, '');
+        }
+        json.push(obj);
+      });
+      const type =
+        data.name.includes('Extract') === true
+          ? 'extract'
+          : data.name.includes('Component') === true
+          ? 'component'
+          : splitName[0];
+
+      const fileContent = {
+        fileName: data.name,
+        fileType: type,
+        json,
+        uploadDate: new Date(data.modifyTime).toISOString(),
+      };
+      // console.log('fileContent', fileContent);
+      // return state;
+      return http
+        .post({
+          url: configuration.openfnInboxUrl,
+          data: fileContent,
+          maxContentLength: Infinity,
+          maxBodyLength: Infinity,
+        })(state)
+        .then(() => {
+          console.log(`Posted ${data.name} to OpenFn Inbox.\n`);
+          return { configuration, references: [] };
+        });
+    });
+  })
+);
