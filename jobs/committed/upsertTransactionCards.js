4880d0810813bab92038fb57da77d3926dad1062
mamadou <mamadou.lakhassane@gmail.com>
2021-06-04 13:40:22 +0000

    transform date to valid syntax

--- jobs/committed/upsertTransactionCards.js
+++ jobs/committed/upsertTransactionCards.js
@@ -13,14 +13,24 @@ beta.each(
       field('CurrencyIsoCode', 'GBP - British Pound'),
       field('npe01__Payment_Method__c', 'Credit Card'),
       field('npe01__Paid__c', true),
-      field('npe01__Payment_Date__c', dataValue('Transaction Date')), // Field present twice
+      field('npe01__Payment_Date__c', state => {
+        let date = dataValue('Transaction Date')(state);
+        date = date.split(' ')[0];
+        const parts = date.match(/(\d+)/g);
+        return new Date(parts[2], parts[1] - 1, parts[0]);
+      }), // Field present twice
       field('npe01__Payment_Amount__c', dataValue('Amount')),
       field('npsp__Payment_Acknowledgment_Status__c', state => {
         return dataValue('Status')(state) === 'Paid' ? 'Acknowledged' : dataValue('Status')(state);
       }),
       relationship('Opportunity_Primary_Campaign_Source__r', 'Source_Code__c', dataValue('PromoCode')),
       field('wfw_Credit_Card_Type__c', dataValue('CCType')),
-      field('npe01__Payment_Date__c', dataValue('SettlementDate')) // Field present twicei
+      field('npe01__Payment_Date__c', state => {
+        let date = dataValue('SettlementDate')(state);
+        date = date.split(' ')[0];
+        const parts = date.match(/(\d+)/g);
+        return new Date(parts[2], parts[1] - 1, parts[0]);
+      }) // Field present twice
     )
   )
 );
