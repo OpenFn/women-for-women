dea4e9b0bfb088b498b0ca8d6c48ed2d809cc08f
Aleksa Krolls <aleksagk@gmail.com>
2021-06-06 15:32:07 +0300

    hawa@openfn.org's change on OpenFn.org

--- jobs/committed/upsertDirectDebits.js
+++ jobs/committed/upsertDirectDebits.js
@@ -20,13 +20,38 @@ beta.each(
       field('npsp__ClosedReason__c', dataValue('CancelReason')),
       field('npe03__Installment_Period__c', dataValue('PaymentFrequency')),
       //field('npsp__InstallmentFrequency__c', dataValue('PaymentFrequency')), //Remove mapping?
-      field('npsp__StartDate__c', state => { 
-        return new Date(dataValue('StartDate')(state)).toISOString();//not working; returning RangeError
+      //field('npsp__StartDate__c', state => { 
+        //return new Date(dataValue('StartDate')(state)).toISOString();//not working; returning RangeError, changed to ISO as below
+     // }),
+       field('npsp__StartDate__c', state => {
+        let date = dataValue('StartDate')(state);
+        date = date.split(' ')[0];
+        const parts = date.match(/(\d+)/g);
+        return new Date(parts[2], parts[1] - 1, parts[0]).toISOString();
       }),
-      field('npe03__Date_Established__c', dataValue('AddedDateTime')),
+      //field('npe03__Date_Established__c', dataValue('AddedDateTime')),// changed to ISO as below
+      field('npe03__Date_Established__c', state => {
+        let date = dataValue('AddedDateTime')(state);
+        date = date.split(' ')[0];
+        const parts = date.match(/(\d+)/g);
+        return new Date(parts[2], parts[1] - 1, parts[0]).toISOString();
+      }),
       field('npe03__Installment_Amount__c', dataValue('FirstAmount')),
-      field('npe03__Next_Payment_Date__c', dataValue('NextDate')),
-      field('npsp__EndDate__c', dataValue('EndDate')),
+     // field('npe03__Next_Payment_Date__c', dataValue('NextDate')),// changed to ISO as below
+      field('npe03__Next_Payment_Date__c', state => {
+        let date = dataValue('NextDate')(state);
+        date = date.split(' ')[0];
+        const parts = date.match(/(\d+)/g);
+        return new Date(parts[2], parts[1] - 1, parts[0]).toISOString();
+      }),
+      
+     // field('npsp__EndDate__c', dataValue('EndDate')),// changed to ISO as below
+      field('npsp__EndDate__c', state => {
+        let date = dataValue('EndDate')(state);
+        date = date.split(' ')[0];
+        const parts = date.match(/(\d+)/g);
+        return new Date(parts[2], parts[1] - 1, parts[0]).toISOString();
+      }),
       field('of_Sisters_Requested__c', dataValue('Number of sponsorships')),
       field('Committed_Giving_Direct_Debit_Reference__c', dataValue('DDRefforBank'))
     )
