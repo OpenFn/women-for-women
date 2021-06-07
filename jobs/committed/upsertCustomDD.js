ff8ca1c68b71bd4ae2c05962654909eda222a3e3
mamadou <mamadou.lakhassane@gmail.com>
2021-06-07 14:31:21 +0000

    build a new array of json array grouping with DDID, #52

--- jobs/committed/upsertCustomDD.js
+++ jobs/committed/upsertCustomDD.js
@@ -1,47 +1,50 @@
-upsert(
-  'Opportunity',
-  'CG_Direct_Debit_ID__c', //Change to CG_Direct_Debit_ID__c ?
-  fields(
-    field('CG_Direct_Debit_ID__c', dataValue('DDID')), //SHould this be CG_Direct_Debit_ID__c ?
-    field('npsp__Tribute_Type__c', state => {
-      const obj = state.data.json.find(js => js.FieldName === 'FormName');
-      return obj ? obj['Value'] : '';
-    }),
-    field('npsp__Honoree_Name__c', state => {
-      const obj = state.data.json.find(js => js.FieldName === 'Honouree / Tributee Name');
-      return obj ? obj['Value'] : '';
-    }),
-    field('wfw_Honoree_City__c', state => {
-      const obj = state.data.json.find(js => js.FieldName === 'Notify Town');
-      return obj ? obj['Value'] : '';
-    }),
-    field('wfw_Honoree_Zip__c', state => {
-      const obj = state.data.json.find(js => js.FieldName === 'Notify Postcode');
-      return obj ? obj['Value'] : '';
-    }),
-    field('wfw_Honoree_State__c', state => {
-      const obj = state.data.json.find(js => js.FieldName === 'Notify County');
-      return obj ? obj['Value'] : '';
-    }),
-    field('wfw_Honoree_Country__c', state => {
-      const obj = state.data.json.find(js => js.FieldName === 'Notify Country');
-      return obj ? obj['Value'] : '';
-    }),
-    field('npsp__Honoree_Contact__c', state => {
-      const obj = state.data.json.find(js => js.FieldName === 'Notify Name');
-      return obj ? obj['Value'] : '';
-    }),
-    field('Tribute_Occasion_Text__c', state => {
-      const obj = state.data.json.find(js => js.FieldName === 'Occasion');
-      return obj ? obj['Value'] : '';
-    }),
-    field('wfw_Honoree_Address_1__c', state => {
-      const obj = state.data.json.find(js => js.FieldName === 'Notify Add1');
-      return obj ? obj['Value'] : '';
-    }),
-    field('Honoree_Address_2__c', state => {
-      const obj = state.data.json.find(js => js.FieldName === 'Notify Add2');
-      return obj ? obj['Value'] : '';
-    })
+alterState(state => {
+  const { json } = state.data;
+  const newJson = [];
+
+  function reduceArray(array) {
+    const arr = array.reduce((r, a) => {
+      r[a.DDID] = r[a.DDID] || [];
+      r[a.DDID].push(a);
+
+      return r;
+    }, Object.create(null));
+    return arr;
+  }
+
+  const group = reduceArray(json);
+  for (let obj in group) {
+    const newObj = {};
+    group[obj].forEach(ob => {
+      if (ob.FieldName) newObj[ob.FieldName] = ob.Value;
+    });
+    newObj['DDID'] = obj;
+    newJson.push(newObj);
+  }
+
+  // state.group = group;
+  state.newJson = newJson;
+  return state;
+});
+
+beta.each(
+  'newJson[*]',
+  upsert(
+    'Opportunity',
+    'CG_Credit_Card_ID__c', //CHANGE TO CG_Credit_Card_ID__c ?
+    fields(
+      field('CG_Credit_Card_ID__c', dataValue('DDID')),
+      field('npsp__Tribute_Type__c', dataValue('FormName')),
+      field('npsp__Honoree_Name__c', dataValue('Honouree / Tributee Name')),
+      field('wfw_Honoree_City__c', dataValue('Notify Town')),
+      field('wfw_Honoree_Zip__c', dataValue('Notify Postcode')),
+
+      field('wfw_Honoree_State__c', dataValue('Notify County')),
+      field('wfw_Honoree_Country__c', dataValue('Notify Country')),
+      field('npsp__Honoree_Contact__c', dataValue('Notify Name')),
+      field('Tribute_Occasion_Text__c', dataValue('Occasion')),
+      field('wfw_Honoree_Address_1__c', dataValue('Notify Add1')),
+      field('Honoree_Address_2__c', dataValue('Notify Add2'))
+    )
   )
 );
