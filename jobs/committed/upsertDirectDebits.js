7acd0c7264a1bd8b5d8d7cb93e1d257fa7020f89
Aleksa Krolls <aleksagk@gmail.com>
2021-06-07 20:16:59 +0300

    hawa@openfn.org's change on OpenFn.org

--- jobs/committed/upsertDirectDebits.js
+++ jobs/committed/upsertDirectDebits.js
@@ -12,7 +12,7 @@ beta.each(
       field('Type__c', state => {
         return dataValue('CampaignCode')(state) === 'Sponsorship' ? 'Sponsorship' : 'Recurring Donation';
       }),
-      relationship('npe03__Recurring_Donation_Campaign__r', 'Source_Code__c', dataValue('PromoCode')),
+     //relationship('npe03__Recurring_Donation_Campaign__r', 'Source_Code__c', dataValue('PromoCode')), //_r or _c?
       field('npe03__Amount__c', dataValue('Regular amount')),
       field('Status__c', state => {
         return dataValue('Status')(state) === 'live' ? 'Active' : 'Canceled'; // Advise on more transform choices
