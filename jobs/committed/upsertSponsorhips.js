837eda70d35552eaa24271ee325e35ad7967835b
mamadou <mamadou.lakhassane@gmail.com>
2021-06-14 10:52:58 +0000

    use bulk

--- jobs/committed/upsertSponsorhips.js
+++ jobs/committed/upsertSponsorhips.js
@@ -1,33 +1,29 @@
-beta.each(
-  dataPath('json[*]'),
-  upsert(
-    'Contact',
-    'wfw_Legacy_Supporter_ID__c',
-    fields(
-      field('Committed_Giving_ID__c', dataValue('PrimKey')),
-      field('wfw_Legacy_Supporter_ID__c', dataValue('PersonRef')),
-      field('Salutation', dataValue('Title')),
-      field('FirstName', dataValue('FirstName')),
-      field('LastName', dataValue('Surname')),
-      field('MailingStreet', state => {
-        const address = `${dataValue('Address1')(state)} ${dataValue('Address2')(state)} ${dataValue('Address3')(
-          state
-        )} ${dataValue('Address4')(state)}`;
-        return address;
-      }),
-      field('MailingCity', dataValue('Address5')),
-      field('MailingState', dataValue('Address6')),
-      field('MailingPostalCode', dataValue('Postcode')),
-      field('Email', state => {
-        const email = dataValue('EmailAddress')(state);
-        if (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) return email;
-        return null;
-      }),
-      field('npe01__Preferred_Email__c', state => {
-        const email = dataValue('EmailAddress')(state);
-        if (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) return email;
-        return null;
-      })
-    )
-  )
+bulk(
+  'Contact', // the sObject
+  'upsert', //  the operation
+  {
+    extIdField: 'wfw_Legacy_Supporter_ID__c', // the field to match on
+    failOnError: true, // throw error if just ONE record fails
+    allowNoOp: true, // what is this?
+  },
+  state => {
+    return state.data.json.map(x => {
+      const email = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(x.EmailAddress) ? x.EmailAddress : null;
+      const address = `${x.Address1} ${x.Address2} ${x.Address3} ${x.Address4}`;
+
+      return {
+        Committed_Giving_ID__c: x.PrimKey,
+        wfw_Legacy_Supporter_ID__c: x.PersonRef,
+        Salutation: x.Title,
+        FirstName: x.FirstName,
+        LastName: x.Surname,
+        MailingStreet: address,
+        MailingCity: x.Address5,
+        MailingState: x.Address6,
+        MailingPostalCode: x.Postcode,
+        Email: email,
+        npe01__Preferred_Email__c: email,
+      };
+    });
+  }
 );
