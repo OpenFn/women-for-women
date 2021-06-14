dbdb8952ad3ba36bd19f4e28b41a99903e53b874
mamadou <mamadou.lakhassane@gmail.com>
2021-06-14 12:42:18 +0000

    update if exist otherwise create, #47

--- jobs/committed/upsertCardMaster.js
+++ jobs/committed/upsertCardMaster.js
@@ -15,38 +15,38 @@ beta.each(dataPath('json[*]'), state => {
         field('npe03__Amount__c', dataValue('Amount')),
         //field('Closeout_Date__c', dataValue('EndDate')),// changed to ISO as below
         field('Closeout_Date__c', state => {
-        let date = dataValue('EndDate')(state);
-        if (!date) return '2021-07-01';
-        date = date.split(' ')[0];
-        const parts = date.match(/(\d+)/g);
-        return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
-      }),
+          let date = dataValue('EndDate')(state);
+          if (!date) return '2021-07-01';
+          date = date.split(' ')[0];
+          const parts = date.match(/(\d+)/g);
+          return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
+        }),
         //field('npsp__StartDate__c', dataValue('StartDate')),//changed to ISO as below
         field('npsp__StartDate__c', state => {
-        let date = dataValue('StartDate')(state);
-        if (!date) return null;
-        date = date.split(' ')[0];
-        const parts = date.match(/(\d+)/g);
-        return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
-      }),
-      
+          let date = dataValue('StartDate')(state);
+          if (!date) return null;
+          date = date.split(' ')[0];
+          const parts = date.match(/(\d+)/g);
+          return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
+        }),
+
         //field('npe03__Next_Payment_Date__c', dataValue('NextDate')),//changed to ISO as below
         field('npe03__Next_Payment_Date__c', state => {
-        let date = dataValue('NextDate')(state);
-        if (!date) return null;
-        date = date.split(' ')[0];
-        const parts = date.match(/(\d+)/g);
-        return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
-      }),
+          let date = dataValue('NextDate')(state);
+          if (!date) return null;
+          date = date.split(' ')[0];
+          const parts = date.match(/(\d+)/g);
+          return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
+        }),
         field('Closeout_Reason__c', dataValue('RecurringCancelReason')),
         //field('Closeout_Date__c', dataValue('RecurringCancelDate')) //changed to ISO format as below
         field('Closeout_Date__c', state => {
-        let date = dataValue('RecurringCancelDate')(state);
-        if (!date) return '2021-07-01';
-        date = date.split(' ')[0];
-        const parts = date.match(/(\d+)/g);
-        return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
-      }),
+          let date = dataValue('RecurringCancelDate')(state);
+          if (!date) return '2021-07-01';
+          date = date.split(' ')[0];
+          const parts = date.match(/(\d+)/g);
+          return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
+        })
       )
     )(state);
   } else {
@@ -58,7 +58,7 @@ beta.each(dataPath('json[*]'), state => {
           return `${dataValue('PrimKey')(state)} ${dataValue('CardMasterID')(state)}`;
         }),
         relationship('RecordType', 'Name', 'Individual Giving'), // HARDCODED
-        field('AccountId', '0013K00000jOtMNQA0'), // HARDCODED.  
+        field('AccountId', '0013K00000jOtMNQA0'), // HARDCODED.
         relationship('npsp__Primary_Contact__r', 'Committed_Giving_Id__c', dataValue('PrimKey')),
         field('Name', dataValue('CardMasterID')),
         field('CG_Credit_Card_ID__c', dataValue('CardMasterID')),
@@ -69,59 +69,81 @@ beta.each(dataPath('json[*]'), state => {
           return dataValue('CCExpiry')(state).split('/')[1];
         }),
         field('Transaction_Reference_Id__c', dataValue('TransactionReference')),
-       // field('Transaction_Date_Time__c', dataValue('AddedDateTime')),// changed to ISO as below
+        // field('Transaction_Date_Time__c', dataValue('AddedDateTime')),// changed to ISO as below
         field('Transaction_Date_Time__c', state => {
-        let date = dataValue('AddedDateTime')(state);
-        if (!date) return null;
-        date = date.split(' ')[0];
-        const parts = date.match(/(\d+)/g);
-        return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
-      }),
+          let date = dataValue('AddedDateTime')(state);
+          if (!date) return null;
+          date = date.split(' ')[0];
+          const parts = date.match(/(\d+)/g);
+          return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
+        }),
         //field('npe01__Payment_Date__c', dataValue('AddedDateTime')),//changed to ISO as below-- not in oppotunity but in payment object
-       // field('npe01__Payment_Date__c', state => {
-       // let date = dataValue('AddedDateTime')(state);
+        // field('npe01__Payment_Date__c', state => {
+        // let date = dataValue('AddedDateTime')(state);
         //if (!date) return null;
-      //  date = date.split(' ')[0];
-       // const parts = date.match(/(\d+)/g);
-      //  return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
-    //  }),
+        //  date = date.split(' ')[0];
+        // const parts = date.match(/(\d+)/g);
+        //  return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
+        //  }),
         field('Amount', dataValue('Amount')),
-        field('CurrencyIsoCode', 'GBP'), 
+        field('CurrencyIsoCode', 'GBP'),
         field('StageName', 'Closed Won'),
         //field('CloseDate', dataValue('LastCredited')) //changed to iso as below
-         field('CloseDate', state => {
-         let date = dataValue('LastCredited')(state);
+        field('CloseDate', state => {
+          let date = dataValue('LastCredited')(state);
           if (!date) return '2021-07-01';
           date = date.split(' ')[0];
-           const parts = date.match(/(\d+)/g);
-         return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
-   }),
+          const parts = date.match(/(\d+)/g);
+          return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
+        })
       )
     )(state).then(state => {
-      return upsert(
-        'npe01__OppPayment__c',
-        'Committed_Giving_ID__c',
-        fields(
-          field('Committed_Giving_ID__c', state => {
-            return `${dataValue('PrimKey')(state)} ${dataValue('TransactionReference')(state)}`;
-          }),
-          relationship('npe01__Opportunity__r', 'Committed_Giving_ID__c', state => {
-            return `${dataValue('PrimKey')(state)} ${dataValue('CardMasterID')(state)}`;
-          }),
-          field('CurrencyIsoCode', 'GBP'), 
-          field('npe01__Payment_Method__c', 'Credit Card'),
-          field('npe01__Paid__c', true),
-         // field('npe01__Payment_Date__c', dataValue('AddedDateTime')),// changed to ISO as below
-          field('npe01__Payment_Date__c', state => {
-        let date = dataValue('AddedDateTime')(state);
-        if (!date) return null;
-        date = date.split(' ')[0];
-        const parts = date.match(/(\d+)/g);
-        return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
-      }),
-          field('npe01__Payment_Amount__c', dataValue('Amount'))
-        )
-      )(state);
+      const { PrimKey, TransactionReference } = state.data;
+
+      return query(
+        `SELECT Id FROM npe01__OppPayment__c WHERE Committed_Giving_ID__c = '${PrimKey}${TransactionReference}'`
+      )(state).then(state => {
+        const { totalSize } = state.references[0];
+        let date = dataValue('AddedDateTime')(state) ? dataValue('AddedDateTime')(state).split(' ')[O] : null;
+        let dateField = null;
+        if (date) {
+          const parts = date.match(/(\d+)/g);
+          dateField = parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
+        }
+
+        const opportunity = `${dataValue('PrimKey')(state)} ${dataValue('CardMasterID')(state)}`;
+        const givingID = `${dataValue('PrimKey')(state)} ${dataValue('TransactionReference')(state)}`;
+
+        if (totalSize > 0) {
+          return update(
+            'npe01__OppPayment__c',
+            fields(
+              field('Committed_Giving_ID__c', givingID),
+              relationship('npe01__Opportunity__r', 'Committed_Giving_ID__c', opportunity),
+              field('CurrencyIsoCode', 'GBP'),
+              field('npe01__Payment_Method__c', 'Credit Card'),
+              field('npe01__Paid__c', true),
+              // field('npe01__Payment_Date__c', dataValue('AddedDateTime')),// changed to ISO as below
+              field('npe01__Payment_Date__c', dateField),
+              field('npe01__Payment_Amount__c', dataValue('Amount'))
+            )
+          )(state);
+        } else {
+          return create(
+            'npe01__OppPayment__c',
+            fields(
+              field('Committed_Giving_ID__c', givingID),
+              relationship('npe01__Opportunity__r', 'Committed_Giving_ID__c', opportunity),
+              field('CurrencyIsoCode', 'GBP'),
+              field('npe01__Payment_Method__c', 'Credit Card'),
+              field('npe01__Paid__c', true),
+              // field('npe01__Payment_Date__c', dataValue('AddedDateTime')),// changed to ISO as below
+              field('npe01__Payment_Date__c', dateField),
+              field('npe01__Payment_Amount__c', dataValue('Amount'))
+            )
+          )(state);
+        }
+      });
     });
   }
 });
