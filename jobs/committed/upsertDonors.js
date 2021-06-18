47f6a9aca4915480b4111dcfa753a7183d9d5112
mamadou <mamadou.lakhassane@gmail.com>
2021-06-18 17:18:33 +0000

    add more check for upserting contact, #57

--- jobs/committed/upsertDonors.js
+++ jobs/committed/upsertDonors.js
@@ -99,104 +99,111 @@ beta.each(
                         field('MailingStreet', address),
                         field('MailingCity', dataValue('Address5')),
                         field('MailingState', dataValue('Address6')),
-            field('MailingPostalCode', state => {
-              const zipCode = dataValue('Postcode')(state);
-              if (zipCode && zipCode.length > 20) {
-                state.zipErrors.push(dataValue('Postcode')(state));
-              }
-              return dataValue('Postcode')(state) ? dataValue('Postcode')(state).substring(0, 20) : '';
-            }),
-            field('MailingCountry', dataValue('Country')),
-            field('HomePhone', dataValue('TelNumber1')),
-            //field('npe01__PreferredPhone__c', dataValue('TelNumber1')),
-            field('npe01__PreferredPhone__c', state => {
-              let phone = dataValue('TelNumber1')(state);
-              if (!phone) return phone ? phone : '0';
-            }),
+                        field('MailingPostalCode', zipCode),
+                        field('MailingCountry', dataValue('Country')),
+                        field('HomePhone', dataValue('TelNumber1')),
 
-            field('MobilePhone', dataValue('Tel2Number')),
-            //field('Email', dataValue('EmailAddress')), // Note: comment this line
-            field('Email', state => {
-              const email = dataValue('EmailAddress')(state);
-              if (email && email.substring(email.length - 3, email.length - 1) === 'up') {
-                if (state.dupErrors.indexOf(email.substring(0, email.length - 3)) === -1)
-                  state.dupErrors.push(email.substring(0, email.length - 3));
+                        field('npe01__PreferredPhone__c', phone),
+                        field('MobilePhone', dataValue('Tel2Number')),
+                        field('Email', state => {
+                          if (EmailSF !== null) return undefined;
+                          return email;
+                        }),
+                        field('npe01__Preferred_Email__c', state => {
+                          if (EmailSF !== null) return undefined;
+                          return email;
+                        }),
+                        field('Call_Opt_In__c', OkToPhone),
+                        field('Email_Opt_in__c', OkToEmail),
+                        field('Mail_Opt_in__c', OkToMail),
+                        field('Text_Opt_In__c', TextOptIn),
+                        field('npsp__Deceased__c', Deceased),
+                        field('wfw_Gift_Aid__c', Gift),
+                        field('wfw_Date_of_Declaration_Confirmation__c', state => {
+                          let date = dataValue('Gift Aid date')(state);
+                          if (!date) return null;
+                          date = date.split(' ')[0];
+                          const parts = date.match(/(\d+)/g);
+                          return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
+                        }),
+                        field('wfw_Donor_Source__c ', dataValue('DonorSource'))
+                      )
+                    )(state);
+                  } else {
+                    // A112. If a matching Contact has been found...
+                    state.dupErrorsDifferentNames.push(email);
+                    return state;
+                  }
+                });
+              } else {
+                // A12. If a matching Contact has been found...
+                state.dupErrorsFirstNameAddress.push(`${FirstName}-${address}`);
+                return state;
               }
-              if (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) return email;
-              return `${dataValue('PrimKey')(state)}@incomplete.com`;
-            }),
-            // field('npe01__Preferred_Email__c', dataValue('EmailAddress')), // Note: comment this line
-            field('npe01__Preferred_Email__c', state => {
-              const email = dataValue('EmailAddress')(state);
-              if (email && email.substring(email.length - 3, email.length - 1) === 'up') {
-                if (state.dupErrors.indexOf(email.substring(0, email.length - 3)) === -1)
-                  state.dupErrors.push(email.substring(0, email.length - 3));
-              }
-              if (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) return email;
-              return `${dataValue('PrimKey')(state)}@incomplete.com`;
-            }),
-            field('Call_Opt_In__c', state => {
-              return dataValue('OK to phone')(state) === 'Yes' ? true : false;
-            }),
-            field('Email_Opt_in__c', state => {
-              return dataValue('OK to email')(state) === 'Yes' ? true : false;
-            }),
-            field('Mail_Opt_in__c', state => {
-              return dataValue('Ok to mail')(state) === 'Yes' ? true : false;
-            }),
-            field('Text_Opt_In__c', state => {
-              return dataValue('Text Opt In')(state) === 'Yes' ? true : false;
-            }),
-            field('npsp__Deceased__c', state => {
-              return dataValue('Deceased')(state) === 'Yes' ? true : false;
-            }),
-            field('wfw_Gift_Aid__c', state => {
-              // data type in SF is not boolean
-              return dataValue('Gift Aid Status')(state) === 'True' ? 'Eligible' : 'Not Eligible - Non Tax Payer';
-            }),
-            //field('wfw_Date_of_Declaration_Confirmation__c', dataValue('Gift Aid date')), //changed to ISO format
-            field('wfw_Date_of_Declaration_Confirmation__c', state => {
-              let date = dataValue('Gift Aid date')(state);
-              if (!date) return null;
-              date = date.split(' ')[0];
-              const parts = date.match(/(\d+)/g);
-              return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
-            }),
-            field('wfw_Donor_Source__c ', dataValue('DonorSource'))
-          )
-        )(state);
+            });
+          } else {
+            // A2. If a matching Contact has been found...
+            if (new Date(LastChangedDateTime) > new Date(LastModifiedDate)) {
+              // If CG is more recent than SF
+              upsertCondition = 2; // We update Contact
+              return upsertIf(
+                dataValue('PrimKey'),
+                'Contact',
+                'wfw_Legacy_Supporter_ID__c',
+                fields(
+                  field('Committed_Giving_ID__c', dataValue('PrimKey')),
+                  field('wfw_Legacy_Supporter_ID__c', dataValue('PersonRef')),
+                  field('Salutation', dataValue('Title')),
+                  field('FirstName', dataValue('FirstName')),
+                  field('LastName', dataValue('Surname')),
+                  field('MailingStreet', address),
+                  field('MailingCity', dataValue('Address5')),
+                  field('MailingState', dataValue('Address6')),
+                  field('MailingPostalCode', zipCode),
+                  field('MailingCountry', dataValue('Country')),
+                  field('HomePhone', dataValue('TelNumber1')),
+
+                  field('npe01__PreferredPhone__c', phone),
+                  field('MobilePhone', dataValue('Tel2Number')),
+                  field('Email', state => {
+                    if (EmailSF !== null) return undefined;
+                    return email;
+                  }),
+                  field('npe01__Preferred_Email__c', state => {
+                    if (EmailSF !== null) return undefined;
+                    return email;
+                  }),
+                  field('Call_Opt_In__c', OkToPhone),
+                  field('Email_Opt_in__c', OkToEmail),
+                  field('Mail_Opt_in__c', OkToMail),
+                  field('Text_Opt_In__c', TextOptIn),
+                  field('npsp__Deceased__c', Deceased),
+                  field('wfw_Gift_Aid__c', Gift),
+                  field('wfw_Date_of_Declaration_Confirmation__c', state => {
+                    let date = dataValue('Gift Aid date')(state);
+                    if (!date) return null;
+                    date = date.split(' ')[0];
+                    const parts = date.match(/(\d+)/g);
+                    return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
+                  }),
+                  field('wfw_Donor_Source__c ', dataValue('DonorSource'))
+                )
+              )(state);
+            } else {
+              upsertCondition = 3; // We update contact but only Committed_Giving_ID__c
+              return upsertIf(
+                dataValue('PrimKey'),
+                'Contact',
+                'Committed_Giving_ID__c',
+                fields(field('Committed_Giving_ID__c', dataValue('PrimKey')))
+              )(state);
+            }
+          }
+        });
       } else {
-        const FirstName = records[0].FirstName;
-        const LastName = records[0].LastName;
-        const Mailing = records[0].MailingStreet;
-        const Email = records[0].Email;
-        const TelNumber1 = records[0].HomePhone;
-        const LastModifiedDate = records[0].LastModifiedDate;
-
-        // We build our custom address from the attributes from SF
-        const customAddress = {
-          city: dataValue('Address5')(state),
-          country: dataValue('Country')(state),
-          postalCode: dataValue('Postcode')(state) ? dataValue('Postcode')(state).substring(0, 20) : '',
-          state: dataValue('Address1')(state),
-        };
-        if (Mailing) {
-          delete Mailing.geocodeAccuracy;
-          delete Mailing.latitude;
-          delete Mailing.longitude;
-          delete Mailing.street;
-        }
-        // =======================================================
-
-        const perfectMatch =
-          FirstName === dataValue('FirstName')(state) &&
-          LastName === dataValue('Surname')(state) &&
-          Email === dataValue('EmailAddress')(state) &&
-          TelNumber1 === dataValue('TelNumber1')(state) &&
-          JSON.stringify(Mailing) === JSON.stringify(customAddress);
-
-        if (size === 1 && (perfectMatch || new Date(LastChangedDateTime) > new Date(LastModifiedDate))) {
-          console.log('Match found.');
+        // B. If a matching Contact has been found...
+        if (new Date(LastChangedDateTime) > new Date(LastModifiedDate)) {
+          // If CG is more recent than SF
           return upsertIf(
             dataValue('PrimKey'),
             'Contact',
@@ -210,64 +217,26 @@ beta.each(
               field('MailingStreet', address),
               field('MailingCity', dataValue('Address5')),
               field('MailingState', dataValue('Address6')),
-              field('MailingPostalCode', state => {
-                const zipCode = dataValue('Postcode')(state);
-                if (zipCode && zipCode.length > 20) {
-                  state.zipErrors.push(dataValue('Postcode')(state));
-                }
-                return dataValue('Postcode')(state) ? dataValue('Postcode')(state).substring(0, 20) : '';
-              }),
+              field('MailingPostalCode', zipCode),
               field('MailingCountry', dataValue('Country')),
               field('HomePhone', dataValue('TelNumber1')),
-              // field('npe01__PreferredPhone__c', dataValue('TelNumber1')),// changed to below
 
-              field('npe01__PreferredPhone__c', state => {
-                let phone = dataValue('TelNumber1')(state);
-                if (!phone) return phone ? phone : '0';
-              }),
-
+              field('npe01__PreferredPhone__c', phone),
               field('MobilePhone', dataValue('Tel2Number')),
               field('Email', state => {
-                const email = dataValue('EmailAddress')(state);
-                // if email is duplicated, we add to the errors array
-                if (email && email.substring(email.length - 3, email.length - 1) === 'up') {
-                  if (state.dupErrors.indexOf(email.substring(0, email.length - 3)) === -1)
-                    state.dupErrors.push(email.substring(0, email.length - 3));
-                }
-                if (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) return email;
-                return `${dataValue('PrimKey')(state)}@incomplete.com`;
+                if (EmailSF !== null) return undefined;
+                return email;
               }),
               field('npe01__Preferred_Email__c', state => {
-                const email = dataValue('EmailAddress')(state);
-                if (email && email.substring(email.length - 3, email.length - 1) === 'up') {
-                  if (state.dupErrors.indexOf(email.substring(0, email.length - 3)) === -1)
-                    state.dupErrors.push(email.substring(0, email.length - 3));
-                }
-                if (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) return email;
-                return `${dataValue('PrimKey')(state)}@incomplete.com`;
+                if (EmailSF !== null) return undefined;
+                return email;
               }),
-              field('Call_Opt_In__c', state => {
-                // not in sandbox
-                return dataValue('OK to phone')(state) === 'Yes' ? true : false;
-              }),
-              field('Email_Opt_in__c', state => {
-                return dataValue('OK to email')(state) === 'Yes' ? true : false;
-              }),
-              field('Mail_Opt_in__c', state => {
-                return dataValue('Ok to mail')(state) === 'Yes' ? true : false;
-              }),
-              field('Text_Opt_In__c', state => {
-                //not in sandbox
-                return dataValue('Text Opt In')(state) === 'Yes' ? true : false;
-              }),
-              //field('npsp__Deceased__c', dataValue('Deceased')),// updated to below
-              field('npsp__Deceased__c', state => {
-                return dataValue('Deceased')(state) === 'Yes' ? true : false;
-              }),
-              field('wfw_Gift_Aid__c', state => {
-                return dataValue('Gift Aid Status')(state) === 'True' ? 'Eligible' : 'Not Eligible - Non Tax Payer';
-              }),
-              // field('wfw_Date_of_Declaration_Confirmation__c', dataValue('Gift Aid date')),// changed to ISO format
+              field('Call_Opt_In__c', OkToPhone),
+              field('Email_Opt_in__c', OkToEmail),
+              field('Mail_Opt_in__c', OkToMail),
+              field('Text_Opt_In__c', TextOptIn),
+              field('npsp__Deceased__c', Deceased),
+              field('wfw_Gift_Aid__c', Gift),
               field('wfw_Date_of_Declaration_Confirmation__c', state => {
                 let date = dataValue('Gift Aid date')(state);
                 if (!date) return null;
@@ -275,7 +244,6 @@ beta.each(
                 const parts = date.match(/(\d+)/g);
                 return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
               }),
-
               field('wfw_Donor_Source__c ', dataValue('DonorSource'))
             )
           )(state);
@@ -288,7 +256,6 @@ beta.each(
             fields(field('Committed_Giving_ID__c', dataValue('PrimKey')))
           )(state);
         }
-        return { ...state, references: [] };
       }
     });
   })
