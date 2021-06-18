35b6f40103bea0509bbf48f6685e011d5962a044
mamadou <mamadou.lakhassane@gmail.com>
2021-06-18 17:17:52 +0000

    if duplicated email, throw errors, implements conditions for check, #57

--- jobs/committed/upsertDonors.js
+++ jobs/committed/upsertDonors.js
@@ -11,36 +11,94 @@ beta.each(
   alterState(state => {
     const { PersonRef, LastChangedDateTime } = state.data;
 
+    let upsertCondition = 0;
+
+    const address = `${dataValue('Address1')(state)} ${dataValue('Address2')(state)} ${dataValue('Address3')(
+      state
+    )} ${dataValue('Address4')(state)}`;
+
+    if (email && email.substring(email.length - 3, email.length - 1) === 'up') {
+      // This means it is a duplicated email
+      if (state.dupErrors.indexOf(email.substring(0, email.length - 3)) === -1)
+        state.dupErrors.push(email.substring(0, email.length - 3));
+
+      throw new Error('Duplicated email found.');
+    }
+
+    // DATA CLEANING ========================================================
+    let zipCode = dataValue('Postcode')(state) || '';
+    if (zipCode && zipCode.length > 20) {
+      state.zipErrors.push(dataValue('Postcode')(state));
+      zipCode = dataValue('Postcode')(state).substring(0, 20);
+    }
+    const phone = dataValue('TelNumber1')(state) ? dataValue('TelNumber1')(state) : '0';
+
+    const OkToPhone = dataValue('OK to phone')(state) === 'Yes' ? true : false;
+    const OkToEmail = dataValue('OK to email')(state) === 'Yes' ? true : false;
+    const OkToMail = dataValue('OK to mail')(state) === 'Yes' ? true : false;
+    const TextOptIn = dataValue('Text Opt In')(state) === 'Yes' ? true : false;
+    const Deceased = dataValue('Deceased')(state) === 'Yes' ? true : false;
+    const Gift = dataValue('Gift Aid Status')(state) === 'True' ? 'Eligible' : 'Not Eligible - Non Tax Payer';
+
+    let email = dataValue('EmailAddress')(state);
+    if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email))
+      email = `${dataValue('PrimKey')(state)}@incomplete.com`;
+
+    // ======================================================================
+
     return query(
-      `SELECT Id, FirstName, LastName, MailingStreet, Email, HomePhone, wfw_Legacy_Supporter_ID__c, LastModifiedDate from CONTACT WHERE wfw_Legacy_Supporter_ID__c = '${PersonRef}'`
+      `SELECT Id, FirstName, LastName, MailingStreet, Email, HomePhone, wfw_Legacy_Supporter_ID__c, LastModifiedDate 
+      FROM CONTACT WHERE wfw_Legacy_Supporter_ID__c = '${PersonRef}'`
     )(state).then(state => {
-      const size = state.references[0].totalSize;
+      const { FirstName, EmailAddress } = state.data;
+      const sizeLegacyMatch = state.references[0].totalSize;
       const { records } = state.references[0];
+      let LastModifiedDate = records[0].LastModifiedDate;
+      const EmailSF = records[0].Email;
 
-      const address = `${dataValue('Address1')(state)} ${dataValue('Address2')(state)} ${dataValue('Address3')(
-        state
-      )} ${dataValue('Address4')(state)}`;
+      if (sizeLegacyMatch === 0) {
+        // A. If no matching Contact has been found...
+        return query(
+          `SELECT Id, FirstName, Email, LastName, MailingStreet, LastModifiedDate 
+          FROM CONTACT WHERE FirstName = '${FirstName}' 
+          AND Email = '${EmailAddress}'`
+        )(state).then(state => {
+          const sizeEmailMatch = state.references[0].totalSize;
+          LastModifiedDate = records[0].LastModifiedDate;
 
-      // If no match
-      if (size === 0) {
-        console.log(`No match found. Creating Contact for wfw_Legacy_Supporter_ID__c: ${PersonRef}`);
-        // When we are here, there is no match so no need to `upsert` ====
-        // A `create` should always be enough ============================
-        // return create(
-        //'Contact',
-        return upsertIf(
-          dataValue('PrimKey'),
-          'Contact',
-          'Committed_Giving_ID__c',
-          fields(
-            field('Committed_Giving_ID__c', dataValue('PrimKey')),
-            field('wfw_Legacy_Supporter_ID__c', dataValue('PersonRef')),
-            field('Salutation', dataValue('Title')),
-            field('FirstName', dataValue('FirstName')),
-            field('LastName', dataValue('Surname')),
-            field('MailingStreet', address),
-            field('MailingCity', dataValue('Address5')),
-            field('MailingState', dataValue('Address6')),
+          if (sizeEmailMatch === 0) {
+            // A1. If no matching Contact has been found...
+            return query(
+              `SELECT Id, FirstName, Email, LastName, MailingStreet, LastModifiedDate 
+              FROM CONTACT WHERE FirstName = '${FirstName}' 
+              AND MailingStreet = '${address}'`
+            )(state).then(state => {
+              const sizeMailingMatch = state.references[0].totalSize;
+
+              if (sizeMailingMatch === 0) {
+                // A11. If no matching Contact has been found...
+                return query(
+                  `SELECT Id, FirstName, Email, LastName, MailingStreet, LastModifiedDate 
+                  FROM CONTACT WHERE Email = '${EmailAddress}'`
+                )(state).then(state => {
+                  const sizeEmailMatch2 = state.references[0].totalSize;
+
+                  if (sizeEmailMatch2 === 0) {
+                    // A111. If no matching Contact has been found...
+                    upsertCondition = 1; // We upsert the new contact on Committed_Giving_ID__c
+                    return upsertIf(
+                      dataValue('PrimKey'),
+                      'Contact',
+                      'Committed_Giving_ID__c',
+                      fields(
+                        field('Committed_Giving_ID__c', dataValue('PrimKey')),
+                        field('wfw_Legacy_Supporter_ID__c', dataValue('PersonRef')),
+                        field('Salutation', dataValue('Title')),
+                        field('FirstName', dataValue('FirstName')),
+                        field('LastName', dataValue('Surname')),
+                        field('MailingStreet', address),
+                        field('MailingCity', dataValue('Address5')),
+                        field('MailingState', dataValue('Address6')),
             field('MailingPostalCode', state => {
               const zipCode = dataValue('Postcode')(state);
               if (zipCode && zipCode.length > 20) {
