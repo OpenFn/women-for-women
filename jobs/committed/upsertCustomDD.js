f8e1929b7ed9caa0fdef0e6123248a4a01bd8c6d
mamadou <mamadou.lakhassane@gmail.com>
2021-06-24 13:18:49 +0000

    bulkify upsertCustomDD, #58

--- jobs/committed/upsertCustomDD.js
+++ jobs/committed/upsertCustomDD.js
@@ -24,34 +24,39 @@ alterState(state => {
 
   // state.group = group;
   state.newJson = newJson;
-  return state;
+
+  return query(`SELECT Id, name from CONTACT`)(state).then(state => {
+    const { records } = state.references[0];
+    const names = records.filter(rec => rec.Id);
+    return { ...state, names };
+  });
 });
 
-beta.each(
-  'newJson[*]',
-  alterState(state => {
-    const name = dataValue('Notify Name')(state);
-    return query(`SELECT Id from CONTACT WHERE name = '${name}'`)(state).then(state => {
-      const { records } = state.references[0];
-
-      const id = records.length > 0 ? records[0].Id : null;
-      return upsert(
-        'Opportunity',
-        'CG_Credit_Card_ID__c', //CHANGE TO CG_Credit_Card_ID__c ?
-        fields(
-          field('CG_Credit_Card_ID__c', dataValue('DDID')),
-          field('npsp__Tribute_Type__c', dataValue('FormName')),
-          field('npsp__Honoree_Name__c', dataValue('Honouree / Tributee Name')),
-          field('wfw_Honoree_City__c', dataValue('Notify Town')),
-          field('wfw_Honoree_Zip__c', dataValue('Notify Postcode')),
-          field('wfw_Honoree_State__c', dataValue('Notify County')),
-          field('wfw_Honoree_Country__c', dataValue('Notify Country')),
-          field('npsp__Honoree_Contact__c', id),
-          field('Tribute_Occasion_Text__c', dataValue('Occasion')),
-          field('wfw_Honoree_Address_1__c', dataValue('Notify Add1')),
-          field('Honoree_Address_2__c', dataValue('Notify Add2'))
-        )
-      )(state);
+bulk(
+  'Opportunity', // the sObject
+  'upsert', //  the operation
+  {
+    extIdField: 'CG_Credit_Card_ID__c', // the field to match on
+    failOnError: true, // throw error if just ONE record fails
+    allowNoOp: true, // what is this?
+  },
+  state => {
+    return state.newJson.map(x => {
+      const name = x['Notify Name'];
+      const contact = state.names.find(rec => rec.Name === name);
+      return {
+        CG_Credit_Card_ID__c: x.DDID,
+        npsp__Tribute_Type__c: x.FormName,
+        npsp__Honoree_Name__c: x['Honouree / Tributee Name'],
+        wfw_Honoree_City__c: x['Notify Town'],
+        wfw_Honoree_Zip__c: x['Notify Postcode'],
+        wfw_Honoree_State__c: x['Notify County'],
+        wfw_Honoree_Country__c: x['Notify Country'],
+        npsp__Honoree_Contact__c: contact ? contact.id : null,
+        Tribute_Occasion_Text__c: x.Occasion,
+        wfw_Honoree_Address_1__c: x['Notify Add1'],
+        Honoree_Address_2__c: x['Notify Add2'],
+      };
     });
-  })
+  }
 );
