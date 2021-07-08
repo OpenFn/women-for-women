dad9976dc70ccaeebf5f5161b0a6d89b81cd50ba
Aleksa Krolls <aleksagk@gmail.com>
2021-07-08 15:34:33 +0200

    aleksa@openfn.org's change on OpenFn.org

--- jobs/committed/upsertTransactionCards.js
+++ jobs/committed/upsertTransactionCards.js
@@ -6,7 +6,7 @@ alterState(state => {
     return parts ? new Date(parts[2], parts[1] - 1, parts[0]).toISOString() : parts;
   };
 
-  const opportunities = state.data.json.map(x => ({ ...x, cgID: `${x.PrimKey}${x.CardMasterID}${x['Transaction Date']}` }));
+  const opportunities = state.data.json.map(x => ({ ...x, cgID: `${x.PrimKey}${x.CardMasterID}${x.Transaction Date}` }));
 
   const cgIDs = opportunities.map(o => `'${o.cgID}'`);
 
@@ -27,7 +27,7 @@ bulk(
       .filter(x => x.PrimKey)
       .map(x => {
         return {
-          Committed_Giving_ID__c: `${x.cgID}`,
+          Committed_Giving_ID__c: `${x.PrimKey}${x.CardMasterID}${x.Transaction Date}`,
           //Committed_Giving_ID__c: `${x.PrimKey}${x.CardMasterID}${x['Transaction Date']}`,
           //Committed_Giving_ID__c: `${x.PrimKey}${x.TransactionReference}${x['Transaction Date']}`, //Wrong UID? 
           //AccountId: '0010n00001E2Z3eAAF', // TODO: Find Contact Account, map here
