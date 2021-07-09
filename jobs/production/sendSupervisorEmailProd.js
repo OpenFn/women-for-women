b8a1cc02824a1c866b3ff6bd18fff1402c50f7c1
Aleksa Krolls <aleksagk@gmail.com>
2021-07-09 22:02:01 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/production/sendSupervisorEmailProd.js
+++ jobs/production/sendSupervisorEmailProd.js
@@ -48,11 +48,11 @@ each(
       return send(
         fields(
           field('from', 'notifications@womenforwomen.org'),
-          //field('to', 'MAverbujA@womenforwomen.org'), //FOR TESTING
-          //field('cc', 'aleksa@openfn.org, jed@openfn.org'), //FOR TESTING
-          field('to', `${state.superEmail}`), //TODO: use when ready to send TO supervisor contact
-          field('cc', `${divisionEmailMap[employee.fields.Division]}`), //TODO: use when ready to copy Division HR contact
-          field('bcc', `maverbuj@womenforwomen.org, mmoisethoams@womenforwomen.org, cani@womenforwomen.org`), //TODO: use for testing
+          field('to', 'MAverbujA@womenforwomen.org'), //FOR TESTING
+          field('cc', 'aleksa@openfn.org, jed@openfn.org'), //FOR TESTING
+          // field('to', `${state.superEmail}`), //TODO: use when ready to send TO supervisor contact
+          // field('cc', `${divisionEmailMap[employee.fields.Division]}`), //TODO: use when ready to copy Division HR contact
+          // field('bcc', `maverbuj@womenforwomen.org, mmoisethoams@womenforwomen.org, cani@womenforwomen.org`), //TODO: use for testing
           field('subject', state => {
             var sub = `New Account: ${state.name} (${state.division})`;
             console.log(sub);
