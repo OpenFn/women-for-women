43d1a9ea258c40f750b27470d06a0108f1acf9b5
aleksa-krolls <aleksagk@gmail.com>
2021-08-20 12:24:28 +0300

    removing Sponsor mapping on RD

--- jobs/committed/upsertCardMasterV2.js
+++ jobs/committed/upsertCardMasterV2.js
@@ -21,15 +21,15 @@ fn(state => {
       'npe03__Contact__r.Committed_Giving_Id__c': x.PrimKey,
       npe03__Installment_Period__c: x.Occurrence,
       npe03__Amount__c: x.Amount,
-     // Closeout_Date__c: formatDate(x.EndDate), // THIS IS DUPLICATED - TO CHECK
+      //Closeout_Date__c: formatDate(x.EndDate), // THIS IS DUPLICATED - TO CHECK
+      Closeout_Date__c: formatDate(x.RecurringCancelDate),
+      Closeout_Reason__c: x.RecurringCancelReason,
       npsp__StartDate__c: formatDate(x.StartDate),
       npe03__Next_Payment_Date__c: formatDate(x.NextDate),
       npsp__PaymentMethod__c: 'Credit Card',
       npe03__Date_Established__c: formatDate(x.AddedDateTime),
-      Closeout_Reason__c: x.RecurringCancelReason,
-      Closeout_Date__c: formatDate(x.RecurringCancelDate), // THIS IS DUPLICATED - TO CHECK
       of_Sisters_Requested__c: x.Occurrence === 'Yearly' ? x.Amount / 264 : x.Amount / 22,
-      'Sponsor__r.Committed_Giving_Id__c': x.PrimKey,
+      //'Sponsor__r.Committed_Giving_Id__c': x.PrimKey,
     };
   };
 
