a805579c5f584beb3e6349d29de5aa4a8039fd0e
mamadou <mamadou@openfn.org>
2021-08-26 12:43:18 +0000

    use new version for formatDate, #65

--- jobs/committed/upsertDirectDebits.js
+++ jobs/committed/upsertDirectDebits.js
@@ -1,49 +1,51 @@
+fn(state => {
+  const formatDate = date => {
+    if (!date) return null;
+    date = date.split(' ')[0];
+    const parts = date.match(/(\d+)/g);
+    const year = String(parts[2]).length > 2 ? parts[2] : `20${parts[2]}`;
+    return parts ? new Date(Number(year), parts[1] - 1, parts[0]).toISOString() : parts;
+  };
+
+  const donations = state.data.json
+    .filter(x => x.PrimKey)
+    .map(x => {
+      return {
+        Committed_Giving_ID__c: `${x.PrimKey}${x.DDId}`,
+        Committed_Giving_Direct_Debit_ID__c: x.DDId,
+        'npe03__Contact__r.Committed_Giving_Id__c': x.PrimKey,
+        'Sponsor__r.Committed_Giving_Id__c': x.PrimKey, //TODO: Overwrite in custom DD mappings?
+        Type__c: x.TransType === 'Sponsorship' ? 'Sponsorship' : 'Recurring Donation',
+        'npe03__Recurring_Donation_Campaign__r.Source_Code__c': x.TransType === 'Sponsorship' ? 'UKSPCC' : 'UKRG',
+        npe03__Amount__c: x['Current amount'],
+        npsp__Status__c: x.Status === 'Live' ? 'Active' : 'Closed',
+        Active__c: x.Status === 'Live' ? true : false, //TO TEST
+        Closeout_Reason__c: x.CancelReason,
+        npe03__Installment_Period__c: x.PaymentFrequency,
+        npe03__Date_Established__c: x.AddedDateTime ? formatDate(x.AddedDateTime) : x.AddedDateTime, //NOTE: Returning 'null' value?
+        //npe03__Next_Payment_Date__c: x.NextDate ? formatDate(x.NextDate) : x.NextDate, //NOTE: Not mapping so that Opps will be auto-inserted for this RD
+        npsp__EndDate__c: x.EndDate ? formatDate(x.EndDate) : x.EndDate,
+        of_Sisters_Requested__c: x['Number of sponsorships'] === ' ' ? undefined : x['Number of sponsorships'],
+        Committed_Giving_Direct_Debit_Reference__c: x.DDRefforBank,
+        npsp__PaymentMethod__c: 'Direct Debit Transfer',
+        Closeout_Date__c: x.CancelDate ? formatDate(x.CancelDate) : x.CancelDate,
+      };
+    });
+
+  return { ...state, donations };
+});
+
 bulk(
   'npe03__Recurring_Donation__c', // the sObject
   'upsert', //  the operation
   {
-    extIdField: 'Committed_Giving_ID__c', // the field to match on
-    failOnError: true, // throw error if just ONE record fails
-    allowNoOp: true, // what is this?
+    extIdField: 'Committed_Giving_ID__c',
+    failOnError: true,
+    allowNoOp: true,
   },
-  state => {
-    const formatDate = date => {
-      console.log('date', date);
-      if (isNaN(new Date(date).getTime())) return null;
-      if (!date) return null;
-      date = date.split(' ')[0];
-      const parts = date.match(/(\d+)/g);
-      const year = String(parts[2]).length > 2 ? parts[2] : `20${parts[2]}`;
-      return parts ? new Date(Number(year), parts[1] - 1, parts[0]).toISOString() : parts;
-    };
-
-    return state.data.json
-      .filter(x => x.PrimKey)
-      .map(x => {
-        return {
-          Committed_Giving_ID__c: `${x.PrimKey}${x.DDId}`,
-          Committed_Giving_Direct_Debit_ID__c: x.DDId,
-          'npe03__Contact__r.Committed_Giving_Id__c': x.PrimKey,
-          'Sponsor__r.Committed_Giving_Id__c': x.PrimKey, //TODO: Overwrite in custom DD mappings? 
-          Type__c: x.TransType === 'Sponsorship' ? 'Sponsorship' : 'Recurring Donation',
-          'npe03__Recurring_Donation_Campaign__r.Source_Code__c': x.TransType === 'Sponsorship' ? 'UKSPCC' : 'UKRG',
-          npe03__Amount__c: x['Current amount'],
-          npsp__Status__c: x.Status === 'Live' ? 'Active' : 'Closed',
-          Active__c: x.Status === 'Live' ? true : false, //TO TEST
-          Closeout_Reason__c: x.CancelReason,
-          npe03__Installment_Period__c: x.PaymentFrequency,
-          npe03__Date_Established__c: x.AddedDateTime ? formatDate(x.AddedDateTime) : x.AddedDateTime, //NOTE: Returning 'null' value?
-          //npe03__Next_Payment_Date__c: x.NextDate ? formatDate(x.NextDate) : x.NextDate, //NOTE: Not mapping so that Opps will be auto-inserted for this RD
-          npsp__EndDate__c: x.EndDate ? formatDate(x.EndDate) : x.EndDate,
-          of_Sisters_Requested__c: x['Number of sponsorships'] === ' ' ? undefined : x['Number of sponsorships'],
-          Committed_Giving_Direct_Debit_Reference__c: x.DDRefforBank,
-          npsp__PaymentMethod__c: 'Direct Debit Transfer',
-          Closeout_Date__c: x.CancelDate ? formatDate(x.CancelDate) : x.CancelDate,
-
-        };
-      });
-  }
+  state => state.donations
 );
+
 // upsert('npe03__Recurring_Donation__c', 'Committed_Giving_ID__c', state => {
 //     const formatDate = date => {
 //       console.log('date', date);
@@ -79,3 +81,7 @@ bulk(
 //       });
 //   })
 
+fn(state => {
+  // lighten state
+  return { ...state, donations: [] };
+});
