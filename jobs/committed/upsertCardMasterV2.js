6fda32c87631426ad496c124b99b924bc2a62031
aleksa-krolls <aleksagk@gmail.com>
2021-08-27 10:16:55 +0300

    revert date mapping

--- jobs/committed/upsertCardMasterV2.js
+++ jobs/committed/upsertCardMasterV2.js
@@ -40,8 +40,9 @@ fn(state => {
       npsp__StartDate__c: formatDate(x.StartDate),
       //
       npsp__PaymentMethod__c: 'Credit Card',
-      npe03__Date_Established__c: formatDate(x.AddedDateTime),
-      npe03__Next_Payment_Date__c: increaseMonth(x.AddedDateTime), //Note: This is required to trigger auto-insert of related Opps
+      npe03__Date_Established__c: increaseMonth(x.AddedDateTime),
+      //npe03__Date_Established__c: formatDate(x.AddedDateTime),
+      //npe03__Next_Payment_Date__c: increaseMonth(x.AddedDateTime), //Note: This is required to trigger auto-insert of related Opps
       //npe03__Next_Payment_Date__c: formatDate(x.NextDate), //TODO: Later update RD to include this? 
       of_Sisters_Requested__c: Number(selectAmount(x)) % 264 === 0 ? Math.abs(x.Amount / 264) : Math.abs(x.Amount / 22),
     };
