43308f5a8f731ddf2447d26812822d3746ce262d
aleksa-krolls <aleksagk@gmail.com>
2021-08-27 12:03:32 +0300

    removing double mapping

--- jobs/committed/upsertCardMasterV2.js
+++ jobs/committed/upsertCardMasterV2.js
@@ -36,7 +36,6 @@ fn(state => {
       npe03__Amount__c: x.Amount,
       Closeout_Date__c: formatDate(x.RecurringCancelDate),
       Closeout_Reason__c: x.RecurringCancelReason,
-      npsp__StartDate__c: formatDate(x.StartDate),
       npsp__PaymentMethod__c: 'Credit Card',
       npe03__Date_Established__c: increaseMonth(x.AddedDateTime),
       npsp__StartDate__c: increaseMonth(x.AddedDateTime),
