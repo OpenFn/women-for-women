afa31efa11f7001d940a540aa4f833f5c5340e63
aleksa-krolls <aleksagk@gmail.com>
2021-09-01 11:17:49 +0300

    updating fields mapping syntax

--- jobs/committed/upsertCustomDDV2.js
+++ jobs/committed/upsertCustomDDV2.js
@@ -44,17 +44,16 @@ each(
         return update(
           'Opportunity',
           fields(
-            field(CG_Credit_Card_ID__c, DDID),
-            field(npsp__Tribute_Type__c, dataValue('FormName')),
-            field(npsp__Honoree_Name__c, dataValue('Honouree / Tributee Name')),
-            field(wfw_Honoree_City__c, dataValue('Notify Town')),
-            field(wfw_Honoree_Zip__c, dataValue('Notify Postcode')),
-            field(wfw_Honoree_State__c, dataValue('Notify County')),
-            field(wfw_Honoree_Country__c, dataValue('Notify Country')),
-            field(npsp__Honoree_Contact__c, dataValue('Notify Name')),
-            field(Tribute_Occasion_Text__c, dataValue('Occasion')),
-            field(wfw_Honoree_Address_1__c, dataValue('Notify Add1')),
-            field(Honoree_Address_2__c, dataValue('Notify Add2'))
+            field('npsp__Tribute_Type__c', dataValue('FormName')),
+            field('npsp__Honoree_Name__c', dataValue('Honouree / Tributee Name')),
+            field('wfw_Honoree_City__c', dataValue('Notify Town')),
+            field('wfw_Honoree_Zip__c', dataValue('Notify Postcode')),
+            field('wfw_Honoree_State__c', dataValue('Notify County')),
+            field('wfw_Honoree_Country__c', dataValue('Notify Country')),
+            field('npsp__Honoree_Contact__c', dataValue('Notify Name')),
+            field('Tribute_Occasion_Text__c', dataValue('Occasion')),
+            field('wfw_Honoree_Address_1__c', dataValue('Notify Add1')),
+            field('Honoree_Address_2__c', dataValue('Notify Add2'))
           )
         )(state);
       }
