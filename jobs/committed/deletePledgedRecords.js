7016aec20da2c0d4e9ebf6b2fe99042a04834068
mamadou <mamadou@openfn.org>
2021-09-01 16:51:46 +0000

    Delete old Pledged CG Opportunities, #85

---
+++ jobs/committed/deletePledgedRecords.js
@@ -0,0 +1,27 @@
+query(
+  `SELECT Id, Committed_Giving_ID__c, closeDate from Opportunity WHERE Committed_Giving_ID__c != null AND StageName = 'Pledged'`
+);
+fn(state => {
+  const { records } = state.references[0];
+  const chunk = (arr, chunkSize) => {
+    var R = [];
+    for (let i = 0, len = arr.length; i < len; i += chunkSize) R.push(arr.slice(i, i + chunkSize));
+    return R;
+  };
+
+  const toDeletes = records.filter(rec => new Date(rec.CloseDate) < new Date());
+  const IDtoDeletes = toDeletes.map(rec => rec.Id);
+  const chunkToDelete = chunk(IDtoDeletes, 200);
+
+  state.chunkToDelete = chunkToDelete;
+  return state;
+});
+
+each('chunkToDelete[*]', state => {
+  return destroy('Opportunity', state => state.data)(state);
+});
+
+fn(state => ({
+  ...state,
+  references: [],
+}));
