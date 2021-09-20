1fab6e95be75e802d5ddd1b84b97426e3f075d5d
Aleksa Krolls <aleksagk@gmail.com>
2021-09-20 08:06:51 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/committed/upsertCardMasterV2.js
+++ jobs/committed/upsertCardMasterV2.js
@@ -40,7 +40,7 @@ fn(state => {
       Name: x.CardMasterID,
       'npe03__Contact__r.Committed_Giving_Id__c': x.PrimKey,
       npe03__Installment_Period__c: Number(selectAmount(x)) % 264 === 0 ? 'Yearly' : 'Monthly',
-      npe03__Amount__c: x.Amount.replace(/�5/g, ''),
+      npe03__Amount__c: x.Amount.replace(/�/, ''),
       Closeout_Date__c: x.RecurringCancelDate ? formatDate(x.RecurringCancelDate) : x.RecurringCancelDate,
       Closeout_Reason__c: x.RecurringCancelReason,
       Active__c: x.RecurringCancelDate ? false : true,
