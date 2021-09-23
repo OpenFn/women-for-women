9ea71e5d58ec29257494469e369b015834201a05
Aleksa Krolls <aleksagk@gmail.com>
2021-09-23 18:31:46 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/committed/1.fetchJSON_Donors.js
+++ jobs/committed/1.fetchJSON_Donors.js
@@ -1,106 +1,106 @@
-alterState(state => {
-    return list('/')(state).then(state => {
-        const fileNames = [
-            'wfwi donors',
-        ];
-        console.log('Files to sync: ', fileNames);
-
-        const files = state.data.filter(
-            file => fileNames.includes(file.name.split('.')[0].toLowerCase()) && file.name.split('.')[1] === 'csv'
-        );
-
-        if (files.length === 0) console.log('No new CSV files found.');
-
-        return { ...state, files };
-    });
-});
-
-each(
-    '$.files[*]',
-    alterState(state => {
-        const { configuration, data } = state;
-
-        const chunk = (arr, chunkSize) => {
-            var R = [];
-            for (var i = 0, len = arr.length; i < len; i += chunkSize) R.push(arr.slice(i, i + chunkSize));
-            return R;
-        };
-
-        return getCSV(`/${data.name}`)(state).then(async state => {
-            const splitName = data.name.split('.');
-            console.log(state.data.length);
-            let json = [];
-            let headers = state.data[0].split(',');
-            headers = headers.map(h => (h = h.replace(/"/g, '')));
-
-            state.data.slice(1).forEach(data => {
-                let row = data.split(',');
-
-                let obj = {};
-                for (let j = 0; j < row.length; j++) {
-                    obj[headers[j]] = row[j].replace(/"/g, '');
-                }
-                json.push(obj);
-            });
-
-            for (i = 0; i < json.length - 1; i++) {
-                let index = [];
-                for (j = i + 1; j < json.length; j++) {
-                    console;
-                    if (json[i]['EmailAddress'] && json[j]['EmailAddress']) {
-                        if (json[i]['EmailAddress'].toLowerCase() === json[j]['EmailAddress'].toLowerCase()) {
-                            index.push(j);
-                        }
-                    }
-                }
-                if (index.length > 0) {
-                    index.forEach((ind, k) => {
-                        json[ind]['EmailAddress'] = `${json[ind]['EmailAddress']}up${k + 1}`;
-                    });
-                }
-            }
-
-            const jsonSets = chunk(json, 50); // chunking into arrays of 50 objects; need smaller Contact batch sizes
-
-            console.log(jsonSets.length, 'sets.');
-
-            const type =
-                data.name.includes('Extract') === true
-                    ? 'extract'
-                    : data.name.includes('Component') === true
-                        ? 'component'
-                        : splitName[0];
-
-            const fileChunks = [];
-            jsonSets.forEach(sets => {
-                const fileContent = {
-                    fileName: data.name,
-                    fileType: type,
-                    json: sets,
-                    uploadDate: new Date(data.modifyTime).toISOString(),
-                    upload: '23-sept-test'
-                };
-                fileChunks.push(fileContent);
-            });
-
-            let countInbox = 0;
-            const postToInbox = async data => {
-                countInbox++;
-                console.log(`${countInbox} request to inbox`);
-
-                await new Promise(resolve => setTimeout(resolve, 2000));
-                await http.post({
-                    url: configuration.openfnInboxUrl,
-                    data,
-                    maxContentLength: Infinity,
-                    maxBodyLength: Infinity,
-                })(state);
-            };
-
-            for (const file of fileChunks) {
-                await postToInbox(file);
-            }
-            return { configuration, references: [], data: {} };
-        });
-    })
-);
+alterState(state => { 
+     return list('/')(state).then(state => { 
+         const fileNames = [ 
+             'wfwi donors', 
+         ]; 
+         console.log('Files to sync: ', fileNames); 
+  
+         const files = state.data.filter( 
+             file => fileNames.includes(file.name.split('.')[0].toLowerCase()) && file.name.split('.')[1] === 'csv' 
+         ); 
+  
+         if (files.length === 0) console.log('No new CSV files found.'); 
+  
+         return { ...state, files }; 
+     }); 
+ }); 
+  
+ each( 
+     '$.files[*]', 
+     alterState(state => { 
+         const { configuration, data } = state; 
+  
+         const chunk = (arr, chunkSize) => { 
+             var R = []; 
+             for (var i = 0, len = arr.length; i < len; i += chunkSize) R.push(arr.slice(i, i + chunkSize)); 
+             return R; 
+         }; 
+  
+         return getCSV(`/${data.name}`)(state).then(async state => { 
+             const splitName = data.name.split('.'); 
+             console.log(state.data.length); 
+             let json = []; 
+             let headers = state.data[0].split(','); 
+             headers = headers.map(h => (h = h.replace(/"/g, ''))); 
+  
+             state.data.slice(1).forEach(data => { 
+                 let row = data.split(','); 
+  
+                 let obj = {}; 
+                 for (let j = 0; j < row.length; j++) { 
+                     obj[headers[j]] = row[j].replace(/"/g, ''); 
+                 } 
+                 json.push(obj); 
+             }); 
+  
+             for (i = 0; i < json.length - 1; i++) { 
+                 let index = []; 
+                 for (j = i + 1; j < json.length; j++) { 
+                     console; 
+                     if (json[i]['EmailAddress'] && json[j]['EmailAddress']) { 
+                         if (json[i]['EmailAddress'].toLowerCase() === json[j]['EmailAddress'].toLowerCase()) { 
+                             index.push(j); 
+                         } 
+                     } 
+                 } 
+                 if (index.length > 0) { 
+                     index.forEach((ind, k) => { 
+                         json[ind]['EmailAddress'] = `${json[ind]['EmailAddress']}up${k + 1}`; 
+                     }); 
+                 } 
+             } 
+  
+             const jsonSets = chunk(json, 50); // chunking into arrays of 1000 objects 
+  
+             console.log(jsonSets.length, 'sets.'); 
+  
+             const type = 
+                 data.name.includes('Extract') === true 
+                     ? 'extract' 
+                     : data.name.includes('Component') === true 
+                         ? 'component' 
+                         : splitName[0]; 
+  
+             const fileChunks = []; 
+             jsonSets.forEach(sets => { 
+                 const fileContent = { 
+                     fileName: data.name, 
+                     fileType: type, 
+                     json: sets, 
+                     uploadDate: new Date(data.modifyTime).toISOString(), 
+                     upload: '23-sept-test' 
+                 }; 
+                 fileChunks.push(fileContent); 
+             }); 
+  
+             let countInbox = 0; 
+             const postToInbox = async data => { 
+                 countInbox++; 
+                 console.log(`${countInbox} request to inbox`); 
+  
+                 await new Promise(resolve => setTimeout(resolve, 2000)); 
+                 await http.post({ 
+                     url: configuration.openfnInboxUrl, 
+                     data, 
+                     maxContentLength: Infinity, 
+                     maxBodyLength: Infinity, 
+                 })(state); 
+             }; 
+  
+             for (const file of fileChunks) { 
+                 await postToInbox(file); 
+             } 
+             return { configuration, references: [], data: {} }; 
+         }); 
+     }) 
+ ); 
