d4158541f6f86a14e3e757f94848334158ff5b2a
Aleksa Krolls <aleksagk@gmail.com>
2021-10-05 19:34:54 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/committed/deletePledgedRecords.js
+++ jobs/committed/deletePledgedRecords.js
@@ -1,5 +1,5 @@
 query(
-  `SELECT Id, npe03__Recurring_Donation__r.Committed_Giving_ID__c, closeDate from Opportunity WHERE npe03__Recurring_Donation__r.Committed_Giving_ID__c != NULL AND StageName = 'Pledged' `
+  `SELECT Id, npe03__Recurring_Donation__r.Committed_Giving_ID__c, Active__c, closeDate from Opportunity WHERE npe03__Recurring_Donation__r.Committed_Giving_ID__c != NULL AND StageName = 'Pledged' `
 );
 fn(state => {
   const { records } = state.references[0];
@@ -12,12 +12,22 @@ fn(state => {
   const toDeletes = records.filter(rec => new Date(rec.CloseDate) < new Date());
   const IDtoDeletes = toDeletes.map(rec => rec.Id);
   const chunkToDelete = chunk(IDtoDeletes, 100);
+  
+  const inactiveOpps = records.filter(rec => rec.Active__c === false);
+  const OppsToDel = inactiveOpps.map(rec => rec.Id);
+  const otherChunkToDel = chunk(OppsToDel, 100);
 
   state.chunkToDelete = chunkToDelete;
+  state.chunkToDelete2 = otherChunkToDel;
+  
   return state;
 });
 
 each('chunkToDelete[*]', state => {
+  //console.log('Opps to delete', state.data);
+  return destroy('Opportunity', state => state.data, { failOnError: false })(state);
+});
+each('chunkToDelete2[*]', state => {
   //console.log('Opps to delete', state.data);
   return destroy('Opportunity', state => state.data, { failOnError: false })(state);
 });
