1b6e77b55444cbd285e5a26e745e7d2dc4faaa2e
Aleksa Krolls <aleksagk@gmail.com>
2021-11-10 17:34:33 +0200

    aleksa@openfn.org's change on OpenFn.org

--- jobs/committed/1.fetchJSON_Donors.js
+++ jobs/committed/1.fetchJSON_Donors.js
@@ -1,6 +1,6 @@
 alterState(state => {
   return list('/')(state).then(state => {
-    const fileNames = ['wfwi donors 29102021'];
+    const fileNames = ['wfwi donors 05112021'];
     console.log('Files to sync: ', fileNames);
 
     const files = state.data.filter(
@@ -78,7 +78,7 @@ each(
           fileType: type,
           json: sets,
           uploadDate: new Date(data.modifyTime).toISOString(),
-          upload: '29-10-2021',
+          upload: '05-11-2021',
         };
         fileChunks.push(fileContent);
       });
