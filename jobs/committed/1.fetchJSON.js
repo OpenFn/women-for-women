b4b8cf3c9a673e33cc4c8840f1190893c37831b4
Aleksa Krolls <aleksagk@gmail.com>
2021-11-29 07:19:53 +0200

    aleksa@openfn.org's change on OpenFn.org

--- jobs/committed/1.fetchJSON.js
+++ jobs/committed/1.fetchJSON.js
@@ -5,7 +5,7 @@ alterState(state => {
       // 'wfwi direct debits 26112021',
       // 'wfwi transactions - cards 26112021',
       // 'wfwi transactions - dd 26112021',
-      'wfwi custom cc fields 26112021',
+      'wfwi custom cc field 26112021',
       //'wfwi custom dd fields 26112021'
     ];
     console.log('Files to sync: ', fileNames);
