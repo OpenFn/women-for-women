c995c3926e5aa77c970ed9fae0f4a4964e533f23
Aleksa Krolls <aleksagk@gmail.com>
2021-12-13 15:08:01 +0200

    aleksa@openfn.org's change on OpenFn.org

--- jobs/committed/1.fetchJSON_Donors.js
+++ jobs/committed/1.fetchJSON_Donors.js
@@ -78,7 +78,7 @@ each(
           fileType: type,
           json: sets,
           uploadDate: new Date(data.modifyTime).toISOString(),
-          upload: '03-12-2021', 
+          upload: '10-12-2021', 
         };
         fileChunks.push(fileContent);
       });
