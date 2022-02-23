a7c2d61a874856f2dd5c1d486dc930b8807e546d
mamadou <mamadou@openfn.org>
2022-02-23 16:15:12 +0000

    use error key to log message, #108

--- jobs/committed/emailErrorAlert.js
+++ jobs/committed/emailErrorAlert.js
@@ -1,24 +1,28 @@
 fn(state => {
-  
-  console.log(state.error);
-  return state;
-})
+  const error = state.error;
+  let errorLine = '';
+  error.forEach(line => {
+    if (line.includes('Potential duplicate')) {
+      errorLine = line;
+    }
+  });
+  return { ...state, errorLine };
+});
 
-/*  fields(
-    field("from", "WfWI Notifications <notifications@womenforwomen.org>"),
-    field("sender", "WfWI Notifications"),
-    field("h:Reply-To", "aleksa@openfn.org"),
-    field("to", "aleksa@openfn.org"), //TODO: update recipients
-    //field('cc', 'aleksa@openfn.org'),
-    field("subject", "Committed Giving Sync: Duplicate Donors Detected"),
-    field(
-      "html",
-      `Dear admins, <br><br> 
+fields(
+  field('from', 'WfWI Notifications <notifications@womenforwomen.org>'),
+  field('sender', 'WfWI Notifications'),
+  field('h:Reply-To', 'aleksa@openfn.org'),
+  field('to', 'aleksa@openfn.org'), //TODO: update recipients
+  //field('cc', 'aleksa@openfn.org'),
+  field('subject', 'Committed Giving Sync: Duplicate Donors Detected'),
+  field(
+    'html',
+    `Dear admins, <br><br> 
 			Duplicate donors were detected in the latest Committed Giving export. Please review the duplicates in Committed Giving to merge the records:
             <br>
-		    ${errorMessageFromParentJob}
+		    ${state.errorLine}
             <br><br> 
             Contact OpenFn at support@openfn.org with any questions.`
-    )
   )
-);*/
+);
