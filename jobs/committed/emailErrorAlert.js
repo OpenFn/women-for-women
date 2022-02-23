92d3b95368648ee6f5c4167e67e46f6ac596fc94
mamadou <mamadou@openfn.org>
2022-02-23 16:18:45 +0000

    send email, #108

--- jobs/committed/emailErrorAlert.js
+++ jobs/committed/emailErrorAlert.js
@@ -9,20 +9,22 @@ fn(state => {
   return { ...state, errorLine };
 });
 
-fields(
-  field('from', 'WfWI Notifications <notifications@womenforwomen.org>'),
-  field('sender', 'WfWI Notifications'),
-  field('h:Reply-To', 'mamadou@openfn.org'),
-  field('to', 'mamadou@openfn.org'), //TODO: update recipients
-  //field('cc', 'aleksa@openfn.org'),
-  field('subject', 'Committed Giving Sync: Duplicate Donors Detected'),
-  field(
-    'html',
-    `Dear admins, <br><br> 
+send(
+  fields(
+    field('from', 'WfWI Notifications <notifications@womenforwomen.org>'),
+    field('sender', 'WfWI Notifications'),
+    field('h:Reply-To', 'mamadou@openfn.org'),
+    field('to', 'mamadou@openfn.org'), //TODO: update recipients
+    //field('cc', 'aleksa@openfn.org'),
+    field('subject', 'Committed Giving Sync: Duplicate Donors Detected'),
+    field(
+      'html',
+      `Dear admins, <br><br> 
 			Duplicate donors were detected in the latest Committed Giving export. Please review the duplicates in Committed Giving to merge the records:
             <br>
 		    ${state.errorLine}
             <br><br> 
             Contact OpenFn at support@openfn.org with any questions.`
+    )
   )
 );
