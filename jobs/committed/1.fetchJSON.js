29ac18a1d32611c9a5bc5ef1caa9dbc850093a37
Aleksa Krolls <aleksagk@gmail.com>
2022-03-07 07:38:05 +0000

    aleksa@openfn.org's change on OpenFn.org

--- jobs/committed/1.fetchJSON.js
+++ jobs/committed/1.fetchJSON.js
@@ -2,7 +2,7 @@ alterState(state => {
   return list('/')(state).then(state => {
     const partialFilenames = [
       'wfwi Card Master 04032022',
-      // 'wfwi Direct Debits 04032022',
+      'wfwi Direct Debits 04032022',
       // 'wfwi Transactions - Cards 04032022',
       // 'wfwi Transactions - DD 04032022',
       // 'wfwi Custom CC Fields 04032022',
