6846a92d2bd69f28b55d95ef7c7f96f136401bea
Aleksa Krolls <aleksagk@gmail.com>
2022-11-14 00:02:24 +0200

    aleksa@openfn.org's change on OpenFn.org

--- jobs/committed/upsertCustomDDV2.js
+++ jobs/committed/upsertCustomDDV2.js
@@ -50,6 +50,7 @@ fn(state => {
               var tribute = dataValue('FormName')(state);
               return tribute ? tribute : 'Honor';
             }),
+            // New mappings :: November 2022
             field('npsp__Honoree_Name__c', dataValue('Honouree / Tributee Name')),
             field('Tribute_Occasion_Text__c', dataValue('Occasion')),
             field('Paper_Card_Shipping_City__c', dataValue('Notify Town')),
@@ -60,6 +61,17 @@ fn(state => {
             field('Paper_Card_Shipping_Address__c', dataValue('Notify Add1')),
             field('Paper_Card_Shipping_Address_Line_2__c', dataValue('Notify Add2')),
             field('eCard_Recipient_Email__c', dataValue('Notify Email Address'))
+            // Mappings in 2021/22
+            // field('npsp__Honoree_Name__c', dataValue('Honouree / Tributee Name')),
+            // field('Tribute_Occasion_Text__c', dataValue('Occasion')),
+            // field('Paper_Card_Shipping_City__c', dataValue('Notify Town')),
+            // field('Paper_Card_Shipping_Zip_Postal__c', dataValue('Notify Postcode')),
+            // field('Paper_Card_Shipping_State_Province__c', dataValue('Notify County')),
+            // field('Paper_Card_Shipping_Country__c', dataValue('Notify Country')),
+            // field('Paper_Card_Shipping_Name__c', dataValue('Notify First Name')),
+            // field('Paper_Card_Shipping_Address__c', dataValue('Notify Add1')),
+            // field('Paper_Card_Shipping_Address_Line_2__c', dataValue('Notify Add2')),
+            // field('eCard_Recipient_Email__c', dataValue('Notify Email Address'))
             //NOTE: These are the original mappings, but the above match CC and page layout... which ones to use?
             // field('wfw_Honoree_City__c', dataValue('Notify Town')),
             // field('wfw_Honoree_Zip__c', dataValue('Notify Postcode')),
