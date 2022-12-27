0b9ca3fc94a676d178a776523d9538e7a50a1b1c
Aleksa Krolls <aleksagk@gmail.com>
2022-12-27 11:27:00 -0700

    aleksa@openfn.org's change on OpenFn.org

--- jobs/committed/upsertDirectDebits.js
+++ jobs/committed/upsertDirectDebits.js
@@ -29,9 +29,9 @@ fn(state => {
         Active__c: x.Status === 'Live' ? true : undefined, //Nov 2022 Request: To not uncheck Active, only add Closeout Date
         Closeout_Reason__c: x.CancelReason,
         npe03__Installment_Period__c: x.PaymentFrequency === 'Annually' ? 'Yearly' : x.PaymentFrequency,
-        //npe03__Date_Established__c: x.AddedDateTime ? formatDate(x.AddedDateTime) : x.AddedDateTime,
-        //npe03__Next_Payment_Date__c: !x.CancelDate ? formatDate(x.NextDate) : undefined,
-        //npsp__EndDate__c: x.EndDate ? formatDate(x.EndDate) : x.EndDate,
+        npe03__Date_Established__c: x.AddedDateTime ? formatDate(x.AddedDateTime) : x.AddedDateTime,
+        npe03__Next_Payment_Date__c: !x.CancelDate ? formatDate(x.NextDate) : undefined,
+        npsp__EndDate__c: x.EndDate ? formatDate(x.EndDate) : x.EndDate,
         of_Sisters_Requested__c: (x['Current amount'] == '22.00' ? 1 : 
         x['Current amount'] % 264 === 0 || (x.PaymentFrequency === 'Annually' && x.TransType === 'Sponsorship') ? Math.floor(Math.abs(x['Current amount'] / 264)) :
             (x.PaymentFrequency === 'Quarterly' && x.TransType === 'Sponsorship') ? Math.floor(Math.abs(x['Current amount'] / 66)) :
@@ -47,7 +47,7 @@ fn(state => {
         //       Number(selectAmount(x)) % 22 === 0 || (x.PaymentFrequency === 'Monthly' && x.TransType === 'Sponsorship') ? Math.floor(Math.abs(x['Current amount'] / 22)) : undefined) : undefined,
         Committed_Giving_Direct_Debit_Reference__c: x.DDRefforBank,
         npsp__PaymentMethod__c: 'Direct Debit',
-        //Closeout_Date__c: x.CancelDate ? formatDate(x.CancelDate) : x.CancelDate,
+        Closeout_Date__c: x.CancelDate ? formatDate(x.CancelDate) : x.CancelDate,
       };
     });
 
