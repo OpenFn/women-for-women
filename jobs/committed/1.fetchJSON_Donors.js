3815215ddc8d0f5fb8fa42f57ced3ca49c17c0d5
mtuchi <mtuchidev@gmail.com>
2023-02-23 18:44:24 +0300

    wip: sort to latest date

--- jobs/committed/1.fetchJSON_Donors.js
+++ jobs/committed/1.fetchJSON_Donors.js
@@ -1,115 +1,148 @@
 list('/');
 
 fn(state => {
-  const fileNames = ['wfwi donors 24022023'];
+  const fileNames = 'wfwi donors';
   console.log('Files to sync: ', fileNames);
 
-  const files = state.data.filter(
-    file => fileNames.includes(file.name.split('.')[0].toLowerCase()) && file.name.split('.')[1] === 'csv'
-  );
+  const diffdate = '17022023';
+
+  const files = state.data
+    .filter(file => {
+      // console.log(file.name.split('.')[0].toLowerCase());
+      // console.log(file.name.split('.')[0].toLowerCase().includes(fileNames));
+      return file.name.split('.')[0].toLowerCase().includes(fileNames) && file.name.split('.')[1] === 'csv';
+    })
+    .map(file => {
+      const dateTime = file.name.split('.')[0].match(/\d+$/);
+
+      console.log(new Date(dateTime));
+      return dateTime && dateTime[0];
+    });
+
+  // .flat()
+  // .sort((a, b) => {
+  //   const distancea = Math.abs(diffdate - a);
+  //   const distanceb = Math.abs(diffdate - b);
+  //   return distancea - distanceb; // sort a before b when the distance is smaller
+  // });
+
+  // console.log(files);
 
   if (files.length === 0) console.log('No new CSV files found.');
 
   return { ...state, files };
 });
 
-each(
-  '$.files[*]',
-  fn(state => {
-    const { configuration, data } = state;
+// fn(state => {
+//   const fileNames = ['wfwi donors 17022023'];
+//   console.log('Files to sync: ', fileNames);
 
-    const chunk = (arr, chunkSize) => {
-      var R = [];
-      for (var i = 0, len = arr.length; i < len; i += chunkSize) R.push(arr.slice(i, i + chunkSize));
-      return R;
-    };
+//   const files = state.data.filter(
+//     file => fileNames.includes(file.name.split('.')[0].toLowerCase()) && file.name.split('.')[1] === 'csv'
+//   );
 
-    const toLowerCase = string => {
-      return string ? string.toLowerCase() : '';
-    };
+//   if (files.length === 0) console.log('No new CSV files found.');
 
-    return getCSV(`/${data.name}`)(state).then(async state => {
-      const splitName = data.name.split('.');
-      console.log(state.data.length);
-      let json = [];
-      let headers = state.data[0].split(',');
-      headers = headers.map(h => (h = h.replace(/"/g, '')));
+//   return { ...state, files };
+// });
 
-      state.data.slice(1).forEach(data => {
-        // let row = data.split(',');
-        let row = data.split(/,(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)/);
+// each(
+//   '$.files[*]',
+//   fn(state => {
+//     const { configuration, data } = state;
 
-        let obj = {};
-        for (let j = 0; j < row.length; j++) {
-          obj[headers[j]] = row[j].replace(/"/g, '');
-        }
-        json.push(obj);
-      });
+//     const chunk = (arr, chunkSize) => {
+//       var R = [];
+//       for (var i = 0, len = arr.length; i < len; i += chunkSize) R.push(arr.slice(i, i + chunkSize));
+//       return R;
+//     };
 
-      const duplicates = [];
-      for (let i = 0; i < json.length; i++) {
-        for (let j = i + 1; j < json.length; j++) {
-          if (json[i]['EmailAddress'] && json[j]['EmailAddress']) {
-            if (json[i]['EmailAddress'].toLowerCase() === json[j]['EmailAddress'].toLowerCase()) {
-              let email = json[i]['EmailAddress'].toLowerCase();
-              if (!duplicates.includes(email)) duplicates.push(email);
-            }
-          }
-        }
-      }
+//     const toLowerCase = string => {
+//       return string ? string.toLowerCase() : '';
+//     };
 
-      const filteredJson = json.filter(js => !duplicates.includes(toLowerCase(js['EmailAddress'])));
+//     return getCSV(`/${data.name}`)(state).then(async state => {
+//       const splitName = data.name.split('.');
+//       console.log(state.data.length);
+//       let json = [];
+//       let headers = state.data[0].split(',');
+//       headers = headers.map(h => (h = h.replace(/"/g, '')));
 
-      json = [];
+//       state.data.slice(1).forEach(data => {
+//         // let row = data.split(',');
+//         let row = data.split(/,(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)/);
 
-      const jsonSets = chunk(filteredJson, 50); // chunking into arrays of 50 objects; need smaller Contact batch sizes
+//         let obj = {};
+//         for (let j = 0; j < row.length; j++) {
+//           obj[headers[j]] = row[j].replace(/"/g, '');
+//         }
+//         json.push(obj);
+//       });
 
-      console.log(jsonSets.length, 'sets.');
+//       const duplicates = [];
+//       for (let i = 0; i < json.length; i++) {
+//         for (let j = i + 1; j < json.length; j++) {
+//           if (json[i]['EmailAddress'] && json[j]['EmailAddress']) {
+//             if (json[i]['EmailAddress'].toLowerCase() === json[j]['EmailAddress'].toLowerCase()) {
+//               let email = json[i]['EmailAddress'].toLowerCase();
+//               if (!duplicates.includes(email)) duplicates.push(email);
+//             }
+//           }
+//         }
+//       }
 
-      const type =
-        data.name.includes('Extract') === true
-          ? 'extract'
-          : data.name.includes('Component') === true
-          ? 'component'
-          : splitName[0];
+//       const filteredJson = json.filter(js => !duplicates.includes(toLowerCase(js['EmailAddress'])));
 
-      const fileChunks = [];
-      jsonSets.forEach(sets => {
-        const fileContent = {
-          fileName: data.name,
-          fileType: type,
-          json: sets,
-          uploadDate: new Date(data.modifyTime).toISOString(),
-          upload: '07042022',
-        };
-        fileChunks.push(fileContent);
-      });
+//       json = [];
 
-      let countInbox = 0;
-      const postToInbox = async data => {
-        countInbox++;
-        console.log(`${countInbox} request to inbox`);
+//       const jsonSets = chunk(filteredJson, 50); // chunking into arrays of 50 objects; need smaller Contact batch sizes
 
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        await http.post({
-          url: configuration.openfnInboxUrl,
-          data,
-          maxContentLength: Infinity,
-          maxBodyLength: Infinity,
-        })(state);
-      };
+//       console.log(jsonSets.length, 'sets.');
 
-      for (const file of fileChunks) {
-        await postToInbox(file);
-      }
+//       const type =
+//         data.name.includes('Extract') === true
+//           ? 'extract'
+//           : data.name.includes('Component') === true
+//           ? 'component'
+//           : splitName[0];
 
-      if (duplicates.length > 0) {
-        console.log('Duplicate rows detected in Committed Giving:');
-        duplicates.map(d => console.log(d));
-        console.log('End of duplicates rows.');
-        throw new Error(`Aborting run; duplicates detected.`);
-      }
-      return { configuration, references: [], data: {} };
-    });
-  })
-);
+//       const fileChunks = [];
+//       jsonSets.forEach(sets => {
+//         const fileContent = {
+//           fileName: data.name,
+//           fileType: type,
+//           json: sets,
+//           uploadDate: new Date(data.modifyTime).toISOString(),
+//           upload: '07042022',
+//         };
+//         fileChunks.push(fileContent);
+//       });
+
+//       let countInbox = 0;
+//       const postToInbox = async data => {
+//         countInbox++;
+//         console.log(`${countInbox} request to inbox`);
+
+//         await new Promise(resolve => setTimeout(resolve, 2000));
+//         await http.post({
+//           url: configuration.openfnInboxUrl,
+//           data,
+//           maxContentLength: Infinity,
+//           maxBodyLength: Infinity,
+//         })(state);
+//       };
+
+//       for (const file of fileChunks) {
+//         await postToInbox(file);
+//       }
+
+//       if (duplicates.length > 0) {
+//         console.log('Duplicate rows detected in Committed Giving:');
+//         duplicates.map(d => console.log(d));
+//         console.log('End of duplicates rows.');
+//         throw new Error(`Aborting run; duplicates detected.`);
+//       }
+//       return { configuration, references: [], data: {} };
+//     });
+//   })
+// );
