5f9c28d9a9c0fa4369c1c2c03520308b4175b6cf
Aleksa Krolls <aleksagk@gmail.com>
2023-05-23 18:16:19 +0200

    itadmin@womenforwomen.org's change on OpenFn.org

--- jobs/production2/upsertUserProd.js
+++ jobs/production2/upsertUserProd.js
@@ -266,7 +266,7 @@ alterState(state => {
     Uganda: 'UG',
     Ukraine: 'UA',
     'United Arab Emirates': 'AE',
-    'United Kingdom': 'UK',
+    'United Kingdom': 'GB',
     'United States': 'US',
     'United States Minor Outlying Islands': 'UM',
     Uruguay: 'UY',
