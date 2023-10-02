4090d84b079fbff33012bc441919d65ab08c0467
Emmanuel Evance <mtuchidev@gmail.com>
2023-10-02 15:01:51 +0300

    update fetchJSON to use filter option

--- jobs/alertFilesNotFound.js
+++ jobs/alertFilesNotFound.js
@@ -1,10 +1,24 @@
-send(state => ({
-  from: 'WfWI Notifications <notifications@womenforwomen.org>',
-  'h:Reply-To': 'aleksa@openfn.org',
-  sender: 'WfWI Notifications',
-  to: 'aissatou@openfn.org, mtuchi@openfn.org',
-  cc: 'aleksa@openfn.org',
-  subject: 'Committed Giving Data Sync: No file found',
-  html: `Dear admins, <br/><br/> Committed Giving file exports may have NOT been uploaded to the SFTP server for date ${state.cursor}: <i>state.filesNotFound</i>
- Please review the sftp server and upload any missing CSV exports. Contact OpenFn at <a href='mailto:support@openfn.org'>support@openfn.org</a> with any questions.`,
-}));
+fn(state => {
+  const { missingDate, missingFiles } = state.data;
+
+  const cc = 'aleksa@openfn.org';
+  const replyTo = 'mtuchi@openfn.org';
+  const to = 'aissatou@openfn.org, mtuchi@openfn.org';
+
+  const email = {
+    from: 'WfWI Notifications <notifications@womenforwomen.org>',
+    'h:Reply-To': replyTo,
+    sender: 'WfWI Notifications',
+    to,
+    cc,
+    subject: 'Committed Giving Data Sync: No file found',
+    html: `Dear admins, <br/><br/> Committed Giving file exports may have NOT been uploaded to the SFTP server for date ${missingDate}: <b>${missingFiles.join(
+      ','
+    )}</b>
+   Please review the sftp server and upload any missing CSV exports. Contact OpenFn at <a href='mailto:support@openfn.org'>support@openfn.org</a> with any questions.`,
+  };
+
+  return { ...state, email };
+});
+
+send(state => state.email);
