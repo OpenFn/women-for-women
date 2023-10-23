d0334dff709ee25eb8b0523521bcdc61ec2ec5f9
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2023-10-23 09:25:14 -0500

    aissatou@openfn.org's change on OpenFn.org

--- jobs/committed/emailErrorAlert.js
+++ jobs/committed/emailErrorAlert.js
@@ -13,7 +13,7 @@ send(state => ({
   from: 'WfWI Notifications <notifications@womenforwomen.org>',
   // 'h:Reply-To': 'aleksa@openfn.org',
   sender: 'WfWI Notifications',
-  to: 'LGreening@womenforwomen.org, rmustakova@womenforwomen.org, jvieyres@womenforwomen.org',
+  to: 'LGreening@womenforwomen.org, rmustakova@womenforwomen.org, jvieyres@womenforwomen.org, klewis@womenforwomen.org',
   cc: 'aleksa@openfn.org',
   subject: 'Committed Giving Data Sync: Duplicate donors detected',
   html: `Dear admins, <br/><br/>
